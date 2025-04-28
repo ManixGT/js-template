6e34adebdbb97feedfda5a448f977ea7bd88f3c6
Suryamani Gupta <manigupta28201@gmail.com>
2025-04-29 01:20:09 +0530

    Setup my folder and going to start from Promises In Js

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-function init() {
-    const executorFn = (resolve, reject) => {
-        resolve(10);
-    }
-    return new Promise(executorFn);
-};
-
-const instance = init();
-
-instance.then(v => {
-    console.log("Value is: ", v);
-});
+console.log('Inital Console.log');
